efe5056b9082eec0a069a1de8cdf8eae90ad5d3f
Miriam Aarag <miriam.aarag@kcl.ac.uk>
2021-04-07 14:11:54 +0200

    Updated job script to be optimistic.

--- venv/run.sh
+++ venv/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
+#SBATCH --account=TaxonomicClassification
 #SBATCH --job-name=taconomic_classification
+#SBATCH --partition=optimist
 #SBATCH --NODES=1
-#SBATCH --time=0-00:01:00
+#SBATCH --time=0-00:05:00
 #SBATCH --mem-per-cpu=1500MB
 
 python main.py
